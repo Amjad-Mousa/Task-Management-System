54e12ef994aeafebd6aad651227697cb0568c44d
Amjad-Mousa <amjad.mousa.600@gmail.com>
2025-05-10 10:33:21 +0300

    some edits

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import {
+  AdminHome,
+  AdminProjects,
+  AdminTasks,
+  AdminChat,
+} from "./components/admin";
+import { StudentHome, StudentTask, StudentChat } from "./components/student";
+import { DarkModeProvider } from "./Context/DarkModeContext";
+
+function App() {
+  return (
+    <DarkModeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/home" element={<AdminHome />} />
+          <Route path="/projects" element={<AdminProjects />} />
+          <Route path="/tasks" element={<AdminTasks />} />
+          <Route path="/chat" element={<AdminChat />} />
+          <Route path="/stu-home" element={<StudentHome />} />
+          <Route path="/student-task" element={<StudentTask />} />
+          <Route path="/student-chat" element={<StudentChat />} />
+        </Routes>
+      </Router>
+    </DarkModeProvider>
+  );
+}
+
+export default App;
